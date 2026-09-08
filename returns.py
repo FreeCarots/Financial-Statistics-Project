be2d966641d90e6bb2fa12f0a6e5e8c1b02d2cf3
FreeCarots <gercdestroyer69@gmail.com>
2026-09-07 23:30:51 -0400

    added Log returns to returns.py

--- returns.py
+++ returns.py
@@ -1,10 +1,20 @@
+import math
 
-# Calculates a simple return percentage
+
+# calculates a simple return value
 def simple_return(price_old, price_new):
 
-    # Prevents Divide by Zero Error
+    # prevents divide by zero error
     if price_old == 0:
         raise ValueError("price_old cannot be zero")
 
     return (price_new - price_old) / price_old
 
+# calculates a log return value
+def log_return(price_old, price_new):
+
+    # prevents divide by zero error
+    if (price_old <= 0) or (price_new <= 0):
+        raise ValueError("price_old and price_new cannot be less than or equal to zero")
+
+    return math.log(price_new / price_old)
